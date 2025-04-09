2bd5220c93cb931f1cd8cbe7d58d5290a5542aa0
Jagaran <jagaran@Jagarans-MacBook-Pro.local>
2025-04-09 09:28:00 +0545

    initial commit with product listing and cart listing

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/product_provider.dart';
+import 'providers/cart_provider.dart';
+import 'screens/product_list_screen.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Product App',
+      debugShowCheckedModeBanner: false,
+      home: ProductListScreen(),
+    );
+  }
+}
---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+import '../models/product.dart';
+
+class CartProvider with ChangeNotifier {
+  final List<Product> _items = [];
+
+  List<Product> get items => _items;
+
+  void addToCart(Product product) {
+    _items.add(product);
+    notifyListeners();
+  }
+
+  void removeFromCart(Product product) {
+    _items.remove(product);
+    notifyListeners();
+  }
+
+  int get itemCount => _items.length;
+}
